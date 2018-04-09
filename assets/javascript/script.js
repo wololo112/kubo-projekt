cf7e39d1b03d2f048976fdf78299f30b17494d2d
shifu13 <simon.darwin@live.dk>
2018-04-09 12:40:06 +0200

    Tilføjelser
    :)

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,3 +6,19 @@ function Publicher { <!--Tester rundt-->
 function load() { { <!--Tester rundt-->
   var storageValue = localStorage.getItem("opgavetitel");
 }
+
+<!--Dette er en local storage funktion-->
+
+function getData() {
+  var name = document.getElementById("opgavetitel").value;
+  var name = document.getElementById("emneord").value;
+  var name = document.getElementById("lektionslængde").value;
+  var name = document.getElementById("beskrivelse").value;
+
+  console.log("opgavetitel");
+
+  alert("Name: " + opgavetitel + emneord + lektionslængde + beskrivelse);
+
+  document.getElementById("succesbox").style.display="block";
+  document.getElementById("succesbox").innerHTML="Welcome: " + name + " You have now logged in succesfully"
+}
