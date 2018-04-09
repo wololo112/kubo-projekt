0f50081bd9f361fee0416b2ab4894010936717b0
shifu13 <simon.darwin@live.dk>
2018-04-09 04:12:40 +0200

    vi prøver igen

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,8 @@
-function Publicher {
+function Publicher { <!--Tester rundt-->
   var fieldValue = document.getElementById("opgavetitel").value
   localStorage.setItem('opgavetitel', fieldValue);
 }
 
-function load() {
+function load() { { <!--Tester rundt-->
   var storageValue = localStorage.getItem("opgavetitel");
 }
